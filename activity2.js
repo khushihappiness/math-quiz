1be5dffb7e17dd38ec82be306241bbcc26b46afa
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-04-04 13:50:46 +0530

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,10 @@
+function back()
+{
+    window.location="activity_1.html";
+}
+function getscore()
+{
+ 
+    score= localStorage.getItem("score");
+    document.getElementById("Update").innerHTML
+}
